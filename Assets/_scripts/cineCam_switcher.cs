c16f997e15ddf46d8ce1f3325894d1b214deddc3
toxxic_mainMachine <chambre12@gmail.com>
2026-06-02 17:27:46 +0200

    camSwitcher

---
+++ Assets/_scripts/cineCam_switcher.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class cineCam_switcher : MonoBehaviour
+{
+    public GameObject[] _cams;
+
+    [Range(0, 10)]
+    public int _activeCam=0;
+
+    private int currentCam = 0;
+
+    void Start()
+    {
+        disableAllCams();
+        _cams[0].SetActive(true);
+    }
+
+    void Update()
+    {
+        if(_activeCam != currentCam)
+        {
+            disableAllCams();
+            
+            _cams[_activeCam].SetActive(true);
+            currentCam = _activeCam;
+        }
+    }
+
+    public void disableAllCams()
+    {
+        for (int i = 0; i < _cams.Length; i++)
+        {
+            _cams[i].SetActive(false);
+        }
+    }
+}
