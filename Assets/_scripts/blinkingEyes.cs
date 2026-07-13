cf3ce4470c2ad7ca91a9b50ff69ce3bf7f26f458
toxxic_mainMachine <chambre12@gmail.com>
2026-07-13 17:18:35 +0200

    blinking eyes

---
+++ Assets/_scripts/blinkingEyes.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using System.Collections;
+using UnityEditor;
+public class blinkingEyes : MonoBehaviour
+{
+    public SkinnedMeshRenderer _skinnedMesh;
+    public int eyeCloseBlendShapeIndex=19;
+
+    [Header("Reglages")]
+    public float speedOpenEyes = 1.0f;
+    public float minValue = 30.0f;
+    public float maxValue = 100.0f;
+
+    void Start()
+    {
+        StartCoroutine(randomBlink());
+    }
+
+    void Update()
+    {
+        if(_skinnedMesh.GetBlendShapeWeight(eyeCloseBlendShapeIndex) >0.0f)
+        {
+            _skinnedMesh.SetBlendShapeWeight(eyeCloseBlendShapeIndex, _skinnedMesh.GetBlendShapeWeight(eyeCloseBlendShapeIndex)- speedOpenEyes);
+        }
+    }
+
+    private IEnumerator randomBlink()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(2.0f, 6.0f));
+
+            //animate blend shape here
+            _skinnedMesh.SetBlendShapeWeight(eyeCloseBlendShapeIndex, Random.Range(minValue, maxValue));
+        }
+    }
+}
